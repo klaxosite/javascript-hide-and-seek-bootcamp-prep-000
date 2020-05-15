0bc380663579cfd652f06cffdb36ecfc6c2d3295
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 04:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 }
 
 function deepestChild(){
-  return document.querySelector('#grand-node div div div div')
+  return document.querySelectorAll('#grand-node div div div div')
 
 
 }
