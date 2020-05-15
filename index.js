9e8177b1a6165e4616488351b3dc06f0c21872d6
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 04:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+
+}
+
+function deepestChild(){
+  return document.querySelectorAll('div#grand-node div div div div')
+
+
+}
+
+function increaseRankBy(n){
+  
+  
+}
