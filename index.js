4bba8f55761bd809816b69564f775c71ac996491
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 04:57:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+
+
+}
+
+function increaseRankBy(n){
+  const ranks = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+  
+  
+}
